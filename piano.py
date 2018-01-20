acc4c0300e281601b0e0192b8c64009f30fb581e
Jakub Sevcik <jakub@sevcik.co>
2018-01-20 15:20:10 +0100

    changed way of terminating processes

--- piano.py
+++ piano.py
@@ -1,15 +1,14 @@
 import os
 import glob
-import time
-import threading
 import subprocess
+import collections
 
 
 DEVNULL = open(os.devnull, 'wb')
 
 
 class Piano(object):
-    TERMINATE_DELAY = 0.3
+    MAX_PARALLEL_PLAY = 4
 
     def __init__(self, tones_dir=None, player='aplay'):
         if tones_dir is None:
@@ -18,27 +17,19 @@ class Piano(object):
         self._tones_dir = tones_dir
         self._player = player
         self._tones = {os.path.basename(name).replace('.wav', '') for name in glob.glob('%s/*.wav' % tones_dir)}
-        self._processes = {}
+        self._processes = collections.deque()
 
     def play(self, tone):
         if tone not in self._tones:
             raise ValueError('Unknown tone %s' % tone)
 
-        self._cancel_previous(tone)
+        self._kill_previous()
 
         cmd = '%s %s/%s.wav' % (self._player, self._tones_dir, tone)
-        p = subprocess.Popen(cmd.split(), stdout=DEVNULL, stderr=subprocess.STDOUT)
-        self._processes[tone] = p
+        process = subprocess.Popen(cmd.split(), stdout=DEVNULL, stderr=subprocess.STDOUT)
+        self._processes.appendleft(process)
 
-    def _cancel_previous(self, tone):
-        prev_process = self._processes.get(tone)
-
-        # previous tone is still playing
-        if prev_process and prev_process.poll() is None:
-            t = threading.Thread(target=self._schedule_terminate, args=(prev_process, ))
-            t.start()
-
-    @classmethod
-    def _schedule_terminate(cls, process):
-        time.sleep(cls.TERMINATE_DELAY)
-        process.kill()
+    def _kill_previous(self):
+        while len(self._processes) >= self.MAX_PARALLEL_PLAY:
+            process = self._processes.pop()
+            process.kill()
